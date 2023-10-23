382b2fdadc7045a39e202eba7f0691671e87fb45
LucienCH <chauvin.lucien.pro@gmail.com>
2023-10-23 11:06:22 +0200

    first window with SDL

--- src/game.c
+++ src/game.c
@@ -23,6 +23,21 @@ int main(int argc, char* args[]) {
         return 1;
     }
 
+    SDL_Renderer* sdl_rend = SDL_CreateRenderer(window,0,0);
+    
+    if(sdl_rend == NULL){
+        printf("Erreur SDL renderer ...");
+        SDL_Quit();
+        return -1;
+    }
+
+    // Creat a rect at pos ( 50, 50 ) that's 50 pixels wide and 50 pixels high.
+    SDL_Rect r;
+    r.x = 50;
+    r.y = 50;
+    r.w = 50;
+    r.h = 50;
+
     // Boucle principale (affiche la fenêtre pendant quelques secondes)
     bool quit = false;
     SDL_Event e;
@@ -33,9 +48,25 @@ int main(int argc, char* args[]) {
                 quit = true;
             }
         }
-
         // Ici, vous pouvez ajouter du code pour dessiner quelque chose sur la fenêtre
         // (ceci est juste un exemple minimal)
+
+        // Set render color to red ( background will be rendered in this color )
+        SDL_SetRenderDrawColor( sdl_rend, 255, 255, 255, 255 );
+
+        // Clear winow
+        SDL_RenderClear(sdl_rend);
+
+
+        // Set render color to blue ( rect will be rendered in this color )
+        SDL_SetRenderDrawColor( sdl_rend, 0, 0, 0, 255 );
+
+        // Render rect
+        SDL_RenderFillRect( sdl_rend, &r );
+
+        // Render the rect to the screen
+        SDL_RenderPresent(sdl_rend);
+
 
         // Attendre pendant une courte période (en millisecondes)
         SDL_Delay(10000); // Attendre 10 seconde
