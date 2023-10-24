55caf59801bbfc5e4cd0bd2d9a532efe5892035c
Antoine <antoine.debrou@edu.ece.fr>
2023-10-24 10:14:49 +0200

    Adding pre-implemented files in the project

--- src/main.c
+++ src/main.c
@@ -3,22 +3,24 @@
 
 #include "MainWindow.h"
 #include "Common.h"
-
+#include "../include/lifegame.h"
 
 int main(int argc, char* args[]){
 
     pthread_t* t_sdl;
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0){
-        printf("Erreur lors de l'initialisation de SDL : %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-
+    // if (SDL_Init(SDL_INIT_VIDEO) != 0){
+    //     printf("Erreur lors de l'initialisation de SDL : %s\n", SDL_GetError());
+    //     exit(EXIT_FAILURE);
+    // }
+    printf("start program \n");
+    output_world();    
+    
     // pthread_create(t_sdl, NULL,(void*)initSDLWindow,NULL);
 
     // pthread_join(t_sdl, NULL);
     
-    initSDLWindow();
+    // initSDLWindow();
 
     return 0;
 }
