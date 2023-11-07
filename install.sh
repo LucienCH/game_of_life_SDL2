8fc00326e9a46899e10f4d9da505ab52fbd9e864
LucienCH <chauvin.lucien.pro@gmail.com>
2023-11-07 09:45:40 +0100

    resolve install conflicts

--- install.sh
+++ install.sh
@@ -5,11 +5,9 @@ git submodule update
 cd SDL
 mkdir build
 cd build
-<<<<<<< HEAD
 ../configure --disable-video-mir
 make -j6
 sudo make install -j6
-
 cd ../../
 mkdir bin
 mkdir obj
@@ -18,8 +16,4 @@ chmod -R 770 obj/
 chown -R $USER:users bin/
 chown -R $USER:users obj/
 export DISPLAY=localhost:0
-=======
-../configure
-make -j4
-sudo make install -j4
->>>>>>> origin/olivia
+
